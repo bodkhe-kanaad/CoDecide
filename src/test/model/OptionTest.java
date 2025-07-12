a8aaf12f6f236222a09faa0f46977bc82dc28d58
kanaad <kanaad@student.ubc.ca>
2025-07-11 23:46:26 -0700

    Added unit test for Option.addVote()

--- src/test/model/OptionTest.java
+++ src/test/model/OptionTest.java
@@ -19,4 +19,17 @@ public class OptionTest {
         assertEquals(newOption.getValue(), testOption.getValue());
     }
 
+    @Test
+    public void testaddVote() {
+        Option newOption = new Option("Test Option");
+        newOption.addVote(15);
+        assertEquals(newOption.getVoteTotal(),15);
+        newOption.addVote(-15);
+        assertEquals(newOption.getVoteTotal(),0);
+        newOption.addVote(0);
+        assertEquals(newOption.getVoteTotal(),0);
+        newOption.addVote(100);
+        assertEquals(newOption.getVoteTotal(),100);
+    }
+
 }
